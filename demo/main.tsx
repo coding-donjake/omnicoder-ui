597eaead0a0bdbff3545d7d3a9a7a8ac874ea086
coding-donjake <108913937+coding-donjake@users.noreply.github.com>
2025-08-14 00:25:29 +0800

    working demo code

---
+++ demo/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import "./style.scss";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-red-500">asdasd</div>
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
